0137e42572eed88caa6f7bf83aa9c0fa9f065263
BowPhatcha <89530388+B6202385@users.noreply.github.com>
2022-02-02 23:00:39 +0700

    Add Validator เหลือทำวันและเวลา

--- backend/entity/examschedule_validator_test.go
+++ backend/entity/examschedule_validator_test.go
@@ -27,8 +27,38 @@ func TestExamSchdulePass(t *testing.T) {
 	g.Expect(err).To(BeNil())
 }
 
-// ตรวจสอบค่าว่างของชื่อแล้วต้องเจอ Error
+//ตรวจสอบปีการศึกษาต้องไม่เป็นตัวเลข 4 หลักต้องเจอ Error 
+func TestAcademicYearMustBeInValidPattern(t *testing.T) {
+	g := NewGomegaWithT(t)
 
+	fixtures := []uint{
+		12345, //ตัวเลข 5 หลัก
+		123, //ตัวเลข 3 หลัก
+		12, //ตัวเลข 2 หลัก
+		1, //ตัวเลข 1 หลัก
+		0,
+	}
+
+	for _, fixture := range fixtures {
+		examschedule := ExamSchedule{
+			AcademicYear: fixture, //ผิด
+			RoomExam: "B5204", 
+		}
+
+		ok, err := govalidator.ValidateStruct(examschedule)
+
+		// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+		g.Expect(ok).ToNot(BeTrue())
+
+		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
+
+		// err.Error ต้องมี error message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("AcademicYear must be 4 digits"))
+	}
+}	
+
+// ตรวจสอบห้องสอบไม่เป็นตามรูปแบบแล้วต้องเจอ Error
 func TestRoomExamMustBeInValidPattern(t *testing.T) {
 	g := NewGomegaWithT(t)
 
@@ -38,7 +68,9 @@ func TestRoomExamMustBeInValidPattern(t *testing.T) {
 		"B00000",   // B ตามด้วย \d 5 ตัว
 		"B000000", // B ตามด้วย \d 6 ตัว
 		"B0000000", // B ตามด้วย \d 7 ตัว
-		"F11-422", // F 
+		"F11-422", // ขึ้นต้น F 
+		"B", //ตัวอักษร 1 ตัว
+		"11111", //มีแต่ตัวเลข
 	}
 
 	for _, fixture := range fixtures {
@@ -56,7 +88,7 @@ func TestRoomExamMustBeInValidPattern(t *testing.T) {
 		g.Expect(err).ToNot(BeNil())
 
 		// err.Error ต้องมี error message แสดงออกมา
-		g.Expect(err.Error()).To(Equal(fmt.Sprintf(`RoomExam: %s does not validate as matches(^[B]\d{4}$)`, fixture)))
+		g.Expect(err.Error()).To(Equal(fmt.Sprintf(`RoomExam: %s does not validate as matches(^[B]\\d{4}$)`, fixture)))
 	}
 	
 
