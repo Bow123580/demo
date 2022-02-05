88e400b1a2c2a239539c68ace20d61e20f855854
BowPhatcha <89530388+B6202385@users.noreply.github.com>
2022-02-05 23:13:46 +0700

    แก้ error element หน่อยค่าาา

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,7 @@
-import React, { useEffect, useState } from "react";
-import clsx from "clsx";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { createStyles, makeStyles, useTheme, Theme, } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Button from "@material-ui/core/Button";
-
-import EventRepeatTwoToneIcon from '@mui/icons-material/EventRepeatTwoTone';
-import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
-import EventNoteTwoToneIcon from '@mui/icons-material/EventNoteTwoTone';
-import BookmarkRemoveTwoToneIcon from '@mui/icons-material/BookmarkRemoveTwoTone';
-import SaveTwoToneIcon from '@mui/icons-material/SaveTwoTone';
-import CreditScoreTwoToneIcon from '@mui/icons-material/CreditScoreTwoTone';
-import AutoStoriesTwoToneIcon from '@mui/icons-material/AutoStoriesTwoTone';
-import SubjectTwoToneIcon from '@mui/icons-material/SubjectTwoTone';
-
+import React,{ useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignIn from "./components/Signin";
-import Registrars from "./components/Registrar";
 import Withdrawals from "./components/Withdrawal";
 import WithdrawalCreate from "./components/WithdrawalCreate";
 import AddCourses from "./components/AddCourses";
@@ -42,103 +14,9 @@ import RecordPetition from "./components/RecordPetition";
 import IncreaseGrades from "./components/IncreaseGrades";
 import IncreaseGradesCreate from "./components/IncreaseGradesCreate";
 
-const drawerWidth = 240; 
+function App() {
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-    },
-    title: {
-      flexGrow: 1,
-    },
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-      transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: 36,
-    },
-    hide: {
-      display: "none",
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-      whiteSpace: "nowrap",
-    },
-    drawerOpen: {
-      width: drawerWidth,
-      transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    drawerClose: {
-      transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      overflowX: "hidden",
-      width: theme.spacing(7) + 1,
-      [theme.breakpoints.up("sm")]: {
-        width: theme.spacing(9) + 1,
-      },
-    },
-    toolbar: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "flex-end",
-      padding: theme.spacing(0, 1),
-      // necessary for content to be below app bar
-      ...theme.mixins.toolbar,
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
-    a: {
-      textDecoration: "none",
-      color: "inherit",
-    },
-  })
-);
-
-export default function MiniDrawer() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
   const [token, setToken] = React.useState<String>("");
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  }; 
-
-  const menu = [
-    { name: "หน้าแรก", icon: <HomeTwoToneIcon color="primary"/>, path: "/" },
-    { name: "เพิ่มรายวิชา", icon: <AutoStoriesTwoToneIcon color="primary" />, path: "/addcourse" },
-    { name: "เพิ่มตารางสอบ", icon: <EventNoteTwoToneIcon color="primary" />, path: "/examschedule" },
-    { name: "คำร้องขอลงทะเบียนเรียนเกินต่ำกว่าหน่วยกิตที่กำหนด", icon: <SubjectTwoToneIcon color="primary" />, path: "/RecordPetition" },
-    { name: "เพิ่มคำร้องขอสอบซ้ำซ้อน", icon: <EventRepeatTwoToneIcon color="primary"/>, path: "/request_exams" },
-    { name: "ขอถอนรายวิชา", icon: <BookmarkRemoveTwoToneIcon color="primary"/>, path: "/withdrawal" },
-    { name: "เพิ่มผลการเรียนนักศึกษา", icon: <CreditScoreTwoToneIcon color="primary" />, path: "/Increasegrade" },
-
-  ];
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -149,105 +27,26 @@ export default function MiniDrawer() {
   if (!token) {
     return <SignIn />;
   }
-
-  const signout = () => {
-    localStorage.clear();
-    window.location.href = "/";
-  };
-
   return (
-    <div className={classes.root}>
-      <Router>
-        <CssBaseline />
-        {token && (
-          <>
-            <AppBar
-              position="fixed"
-              className={clsx(classes.appBar, {
-                [classes.appBarShift]: open,
-              })}
-            >
-              <Toolbar>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={handleDrawerOpen}
-                  edge="start"
-                  className={clsx(classes.menuButton, {
-                    [classes.hide]: open,
-                  })}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                  ระบบลงทะเบียนเรียน
-                </Typography>
-
-                <Button color="inherit" onClick={signout}>
-                  ออกจากระบบ
-                </Button>
-              </Toolbar>
-            </AppBar>
-            <Drawer
-              variant="permanent"
-              className={clsx(classes.drawer, {
-                [classes.drawerOpen]: open,
-                [classes.drawerClose]: !open,
-              })}
-              classes={{
-                paper: clsx({
-                  [classes.drawerOpen]: open,
-                  [classes.drawerClose]: !open,
-                }),
-              }}
-            >
-              <div className={classes.toolbar}>
-                <IconButton onClick={handleDrawerClose}>
-                  {theme.direction === "rtl" ? (
-                    <ChevronRightIcon />
-                  ) : (
-                    <ChevronLeftIcon />
-                  )}
-                </IconButton>
-              </div>
-              <Divider />
-
-              <List>
-                {menu.map((item, index) => (
-                  <Link to={item.path} key={item.name} className={classes.a}>
-                    <ListItem button>
-                      <ListItemIcon>{item.icon}</ListItemIcon>
-                      <ListItemText primary={item.name} />
-                    </ListItem>
-                  </Link>
-                ))}
-              </List>
-            </Drawer>
-          </>
-        )}
-
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <div>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/registrars" component={Registrars} />
-              <Route exact path="/withdrawal" component={Withdrawals} />
-              <Route exact path="/withdrawal/create" component={WithdrawalCreate} />
-              <Route exact path="/addcourse" component={AddCourses} />
-              <Route exact path="/addcourse/create" component={AddCourseCreate} />
-              <Route exact path="/examschedule" component={ExamSchedules} />
-              <Route exact path="/examschedule/create" component={ExamScheduleCreate} />
-              <Route exact path="/request_exams" component={RequestExams} />
-              <Route exact path="/request_exam/create" component={RequestExamCreate}/>
-              <Route exact path="/RecordPetition" component={RecordPetition} />
-              <Route exact path="/Increasegrade" component={IncreaseGrades} />
-              <Route exact path="/Increasegrade/create" component={IncreaseGradesCreate} />
-
-            </Switch>
-          </div>
-        </main>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+              <Route  path="/" element={<Home />} />
+              <Route  path="/withdrawal" element={<Withdrawals />} />
+              <Route  path="/withdrawal/create" element={<WithdrawalCreate />} />
+              <Route  path="/addcourse" element={<AddCourses />} />
+              <Route  path="/addcourse/create" element={<AddCourseCreate />} />
+              <Route  path="/examschedule" element={<ExamSchedules />} />
+              <Route  path="/examschedule/create" element={<ExamScheduleCreate />} />
+              <Route  path="/request_exams" element={<RequestExams/>} />
+              <Route  path="/request_exam/create" element={<RequestExamCreate />}/>
+              <Route  path="/RecordPetition" element={<RecordPetition />} />
+              <Route  path="/Increasegrade" element={<IncreaseGrades />} />
+              <Route  path="/Increasegrade/create" element={<IncreaseGradesCreate />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+export default App;
